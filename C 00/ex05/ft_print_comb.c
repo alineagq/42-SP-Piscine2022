a65fdc5bc0ca6fad9f744a5646e2106efc6f6931
alineagq <alinequeirozagq@gmail.com>
2022-08-07 08:05:19 -0300

    C 00 exercise 05 rebuild

--- C 00/ex05/ft_print_comb.c
+++ C 00/ex05/ft_print_comb.c
@@ -6,46 +6,36 @@
 /*   By: aqueiroz <aqueiroz@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 07:06:58 by aqueiroz          #+#    #+#             */
-/*   Updated: 2022/08/07 07:42:46 by aqueiroz         ###   ########.fr       */
+/*   Updated: 2022/08/07 08:04:07 by aqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	check_value(char c, char d, char u);
-
 void	ft_print_comb(void)
 {
-	char	c;
-	char	d;
-	char	u;
+	char	hundred;
+	char	decimal;
+	char	unity;
 
-	c = '0' - 1;
-	d = '1' - 1;
-	u = '2' - 1;
-	while (c++ < '9')
+	hundred = '0' - 1;
+	while (++hundred <= '9')
 	{
-		while (d++ < '9')
+		decimal = hundred;
+		while (++decimal <= '9')
 		{
-			while (u++ < '9')
+			unity = decimal + 1;
+			while (unity <= '9')
 			{
-				check_value(c, d, u);
-				u++;
+				write(1, &hundred, 1);
+				write(1, &decimal, 1);
+				write(1, &unity, 1);
+				if (hundred != '7' || decimal != '8' || unity != '9')
+				{
+					write(1, ", ", 2);
+				}
+				unity++;
 			}
-			u = '0';
 		}
-		d = '0';
-	}
-}
-
-void	check_value(char c, char d, char u)
-{
-	if (c != d && d != u && u != c && c < d && d < u)
-	{
-		write(1, &c, 1);
-		write(1, &d, 1);
-		write(1, &u, 1);
-		if (! (c == '7' && d == '8' && u == '9'))
-			write(1, ", ", 2);
 	}
 }
